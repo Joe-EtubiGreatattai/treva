edeae9c67b2616a8249311cfe24bcc8cf7ddd062
Great <greatattai442442@gmail.com>
2023-01-01 22:55:54 +0100

    10:55 changes most recetn

--- client/script.js
+++ client/script.js
@@ -1,6 +1,3 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
-
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
@@ -50,69 +47,69 @@ function chatStripe(isAi, value, uniqueId) {
         <div class="wrapper ${isAi ? 'ai' : 'user'}">
             <div class="chat">
                 <div class="message" id=${uniqueId}>${value}</div>
-                    </div>
                 </div>
-            `
-            );
-        }
-        
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-        
-            const data = new FormData(form);
-        
-            // user's chatstripe
-            chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-        
-            // to clear the textarea input 
-            form.reset();
-        
-            // bot's chatstripe
-            const uniqueId = generateUniqueId();
-            chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
-        
-            // to focus scroll to the bottom 
-            chatContainer.scrollTop = chatContainer.scrollHeight;
-        
-            // specific message div 
-            const messageDiv = document.getElementById(uniqueId);
-        
-            // messageDiv.innerHTML = "..."
-            loader(messageDiv);
-        
-            try {
-                const response = await fetch('https://treva.onrender.com', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        prompt: data.get('prompt'),
-                    }),
-                });
-        
-                if (response.ok) {
-                    clearInterval(loadInterval);
-                    messageDiv.innerHTML = ' ';
-        
-                    const data = await response.json();
-                    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n' 
-        
-                    typeText(messageDiv, parsedData);
-                } else {
-                    throw new Error(response.statusText);
-                }
-            } catch (error) {
+            </div>
+        `
+        );
+    }
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const data = new FormData(form);
+    
+        // user's chatstripe
+        chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+    
+        // to clear the textarea input 
+        form.reset();
+    
+        // bot's chatstripe
+        const uniqueId = generateUniqueId();
+        chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+    
+        // to focus scroll to the bottom 
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    
+        // specific message div 
+        const messageDiv = document.getElementById(uniqueId);
+    
+        // messageDiv.innerHTML = "..."
+        loader(messageDiv);
+    
+        try {
+            const response = await fetch('http://localhost:5000', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    prompt: data.get('prompt'),
+                }),
+            });
+    
+            if (response.ok) {
                 clearInterval(loadInterval);
-                messageDiv.innerHTML = 'Something went wrong';
-                alert(error.message);
+                messageDiv.innerHTML = ' ';
+    
+                const data = await response.json();
+                const parsedData = data.bot.trim(); // trims any trailing spaces/'\n' 
+    
+                typeText(messageDiv, parsedData);
+            } else {
+                throw new Error(response.statusText);
             }
-        };
-        
-        form.addEventListener('submit', handleSubmit);
-        form.addEventListener('keyup', (e) => {
-            if (e.keyCode === 13) {
-                handleSubmit(e);
-            }
-        });
-                
+        } catch (error) {
+            clearInterval(loadInterval);
+            messageDiv.innerHTML = 'Oppsies';
+            alert(error.message);
+        }
+    };
+    
+    form.addEventListener('submit', handleSubmit);
+    form.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+            handleSubmit(e);
+    }
+});
+
